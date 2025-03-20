7fba939a58c3427dc5e4aa7bd52e034b13daa03f
TrungBill <118158645+TrungBill@users.noreply.github.com>
2025-03-20 14:25:05 +1100

    updated home button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion'
 
 const Hero = () => {
   return (
-    <div id='#home' className='px-16 flex min-h-screen w-full items-center justify-center py-28 md:px-32'>
+    <div id='home' className='px-16 flex min-h-screen w-full items-center justify-center py-28 md:px-32'>
       <div className='flex flex-col items-center justify-center gap-10 text-white'>
         <motion.div
           initial={{ y: -50, opacity: 0 }}
