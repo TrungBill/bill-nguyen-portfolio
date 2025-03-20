870c76ff295e898c30bbd2c7aada3382daa70163
TrungBill <118158645+TrungBill@users.noreply.github.com>
2025-03-20 13:29:53 +1100

    Added project section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,27 +10,20 @@ const Hero = () => {
           initial={{ y: -50, opacity: 0 }}
           animate={{y: 0, opacity: 1}}
           transition={{ duration: 0.8, delay: 0.2 }}>
-            
-          <img 
-            src={image}  
-            alt='Bill Nguyen' 
-           className="w-[300px] h-[300px]  overflow-hidden rounded-full shadow-xl shadow-gray-900 transition-all duration-300 hover:-translate-y-5 hover:scale-105 hover:shadow-1xl hover:shadow-gray-600"
-          />
+            <h1 className="bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text text-transparent text-5xl font-light leading-relaxed md:text-7xl pb-3">
+                Hi,I'm Bill Nguyen
+          </h1>
+         
         </motion.div>
         <motion.div 
         initial={{ y: 50, opacity: 0 }}
         animate={{y: 0, opacity: 1}}
         transition={{ duration: 0.8, delay: 0.2 }}
         className='flex max-w-[600px] flex-col items-center justify-center gap-1 text-center'>
-          <div className="pb-1"> {/* Wrapper with padding */}
-          <h1 className="bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text text-transparent text-5xl font-light leading-relaxed md:text-7xl pb-3">
-                Bill Nguyen
-          </h1>
+         
+         
 
-          </div>
-          <h3 className="bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text text-transparent text-2xl md:text-3xl mb-1">Computer Science Student</h3>
-
-          <p className="md:text-base text-pretty text-sm text-gray-400">I am a Computer Science student at the Monash University, Melbourne. I am passionate about software development and I am always looking for opportunities to learn and grow.</p>
+          <p className="md:text-base text-pretty text-sm text-gray-400">Dynamic front-end developer and Computer Science undergraduate at Monash University.</p>
         </motion.div>
       </div>
     </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,16 +1,74 @@
 import React from 'react'
 import { motion } from 'framer-motion'
-import portfolio from '../assets/portfolio.png'
-import football from '../assets/football.png'
-import travel from '../assets/travel.png'
+import football from '/public/football.png'
+import portfolio from '/public/portfolio.png'
+import travel from '/public/travel.png'
 
+const projectData = [
+  {
+    image: football,
+    title: 'PitchPulse',
+    description: 'A football chrome extension that displays upcoming fixtures and results of football matches.',
+    technologies: ['Vite','React', 'Tailwind CSS', 'Football.org API', 'Chrome Storage API'],
+  },
+  {
+    image: travel,
+    title: 'Travel Mate',
+    description: 'A travel companion web application that displays local points of interest with real-time weather data and a location-based search to provide quick discovery and detailed info.',
+    technologies: ['Vite','React', 'Tailwind CSS', 'Travel Advisor API', 'Open Weather API']
+  },
 
+  {
+    image: portfolio,
+    title: 'Portfolio Website',
+    description: 'Modern, responsive Portfolio Website for a Business Analytics Student at Deakin University.',
+    technologies: ['Vite','React', 'Tailwind CSS', 'Framer Motion', 'EmailJS' ]
+
+  },
+];
+
+const ProjectCard = ({ project}) => {
+  return(
+    <div className='flex flex-col items-center gap-8 md:flex-row md:gap-24'>
+      <img src={project.image} alt="" className='w-full cursor-pointer rounded-2xl transition-all duration-300 hover:scale-105 md:w-[250px]'/>
+      <div className='flex flex-col gap-5'>
+        <div className='flex flex-col gap-3'>
+          <div className='text-xl font-semibold'>{project.title}</div>
+          <p className='text-gray-400'>{project.description}</p>
+        </div>
+
+        <div className='flex flex-wrap gap-5'>
+          {
+            project.technologies.map((tech, index) => (
+              <span key={index} className='rounded-lg bg-purple p-3'>{tech}</span>
+            ))
+          }
+
+        </div>
+      </div>
+    </div> 
+  )
+}
 
 const Projects = () => {
   return (
-    <div>
-
-
+    <div id="projects" className="flex min-h-screen w-full flex-col items-center justify-center gap-16 p-4 md:px-14 md:py-24">
+      <motion.h1 
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        className="text-4xl font-light text-white md:text-6xl"
+      >
+        Projects
+      </motion.h1>
+      
+      <div className="flex w-full max-w-[1000px] flex-col gap-16 text-white">
+        {
+          projectData.map((project, index) => (
+            <ProjectCard key={index} project={project} />
+          ))
+        }
+      </div>
     </div>
   )
 }
