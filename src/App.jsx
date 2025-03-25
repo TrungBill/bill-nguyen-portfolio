bb333b4f0897c0b3a8c5a637b09df6d7f619b9ca
Quang Trung Nguyen <trungbill04@gmail.com>
2025-03-25 18:33:24 +1100

    Prettify app code

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,15 @@ import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
-import Contact from './components/Contact'
-import Projects from './components/Projects'
 import Tech from './components/Tech'
-
+import Projects from './components/Projects'
+import Contact from './components/Contact'
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false)  // Moved outside JSX
+  const [isLoading, setIsLoading] = useState(false)
   
   return (
     <>
-     
-
-
       <div className="fixed inset-0 -z-10 bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       <main className='flex flex-col items-center px-4 md:px-8 lg:px-16'>
         <Navbar />
