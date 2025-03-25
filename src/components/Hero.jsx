7c9bf97b6c5669ca0bd392a35a30c6e001ebbf08
Quang Trung Nguyen <trungbill04@gmail.com>
2025-03-25 18:21:26 +1100

    Prettify hero code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,31 +8,32 @@ const Hero = () => {
       <div className='flex flex-col items-center justify-center gap-10 text-white'>
         <motion.div
           initial={{ y: -50, opacity: 0 }}
-          animate={{y: 0, opacity: 1}}
+          animate={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.8, delay: 0.2 }}
-          className='w-[300px] h-[300px]'>
-          
-            
+          className='w-[300px] h-[300px]'
+        >
           <img 
             src={image}  
             alt='Bill Nguyen' 
-           className="w-full h-full overflow-hidden rounded-full shadow-xl shadow-gray-900 transition-all duration-300 hover:-translate-y-5 hover:scale-105 hover:shadow-1xl hover:shadow-gray-600"
+            className="w-full h-full overflow-hidden rounded-full shadow-xl shadow-gray-900 transition-all duration-300 hover:-translate-y-5 hover:scale-105 hover:shadow-1xl hover:shadow-gray-600"
           />
         </motion.div>
+        
         <motion.div 
-        initial={{ y: 50, opacity: 0 }}
-        animate={{y: 0, opacity: 1}}
-        transition={{ duration: 0.8, delay: 0.2 }}
-        className='flex max-w-[600px] flex-col items-center justify-center gap-1 text-center'>
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.8, delay: 0.2 }}
+          className='flex max-w-[600px] flex-col items-center justify-center gap-1 text-center'
+        >
           <div className="pb-1"> {/* Wrapper with padding */}
-          <h1 className="bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text text-transparent text-5xl font-light leading-relaxed md:text-7xl pb-3">
-                Hi,I'm Bill Nguyen
-          </h1>
-
+            <h1 className="bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text text-transparent text-5xl font-light leading-relaxed md:text-7xl pb-3">
+              Hi, I'm Bill Nguyen
+            </h1>
           </div>
           
-
-          <p className="md:text-base text-pretty text-sm text-gray-400">Dynamic front-end developer and Computer Science undergraduate at Monash University.</p>
+          <p className="md:text-base text-pretty text-sm text-gray-400">
+            Dynamic front-end developer and Computer Science undergraduate at Monash University.
+          </p>
         </motion.div>
       </div>
     </div>
